6d0f8f263b75603eff7e2afb173dab88ce2ca7b8
Godspeed715 <chimzagamdaniel@gmail.com>
2026-08-13 16:27:06 +0100

    modified products/routers.py
    added a route to return products_with_stock

--- products/routers.py
+++ products/routers.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from products.models import CashoutModel
 from core.database import get_db
-from products.queries import fetch_all_products, fetch_all_stock, perform_transaction
+from products.queries import fetch_all_products, fetch_all_stock, perform_transaction, fetch_all_products_with_stock
 from auth.utils import get_current_user
 
 router = APIRouter(tags=['Products'], prefix='/api')
@@ -12,8 +12,14 @@ async def products(aconn = Depends(get_db), user = Depends(get_current_user)):
     results = await fetch_all_products(aconn)
     return results
 
-@router.get('/stock')
+@router.get('/products_with_stocks')
 async def products(aconn = Depends(get_db), user = Depends(get_current_user)):
+    '''Returns a list of alll the products'''
+    results = await fetch_all_products_with_stock(aconn)
+    return results
+
+@router.get('/stock')
+async def stock(aconn = Depends(get_db), user = Depends(get_current_user)):
     '''Returns a dictionary of all the stocks'''
     results = await fetch_all_stock(aconn)
     return results
