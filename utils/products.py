e43bb551248ce16cb1196be6dd2e41d89e6478a6
Godspeed715 <chimzagamdaniel@gmail.com>
2026-09-09 12:28:54 +0100

    modified utils/products.py
    added a new utility to arange a single dict not a list

--- utils/products.py
+++ utils/products.py
@@ -1,4 +1,4 @@
-def remove_custom_placeholders(products: dict) -> dict:
+def remove_custom_placeholders(products: list[dict]) -> list[dict]:
     for product in products:
         if product['custom_name']!=None:
             product['name'] = product['custom_name']
@@ -8,3 +8,13 @@ def remove_custom_placeholders(products: dict) -> dict:
         del product['custom_name']
         del product['custom_category']
     return products
+
+def remove_custom_placeholder(product: dict) -> dict:
+    if product['custom_name']!=None:
+        product['name'] = product['custom_name']
+    if product['custom_category']!=None:
+        product['category'] = product['custom_category']
+
+    del product['custom_name']
+    del product['custom_category']
+    return product
