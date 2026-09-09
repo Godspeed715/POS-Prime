853f9d6745c864a23f3cbad931ab97bb1e44fa45
Godspeed715 <chimzagamdaniel@gmail.com>
2026-09-09 13:15:26 +0100

    modified queries/transactions.py
    added await to cur
    also renamed the columns being returned

--- queries/transactions.py
+++ queries/transactions.py
@@ -1,4 +1,5 @@
 from psycopg import AsyncConnection
+from psycopg.rows import dict_row
 
 # Later make transactions show users that made the transaction 
 # Enable deleting transactions
@@ -6,11 +7,11 @@ from psycopg import AsyncConnection
 async def fetch_transactions_by_id(aconn: AsyncConnection, business_id: str):
     '''Fetches all transactions for that business'''
     async with aconn.transaction():
-        async with aconn.cursor() as cur:
-            cur.execute('''
-                SELECT t.id, t.user_id, u.username, t.total_amount, t.created_at
+        async with aconn.cursor(row_factory=dict_row) as cur:
+            await cur.execute('''
+                SELECT t.id, t.user_id, u.username AS cashier_username, t.total_amount, t.created_at AS timestamp
                 FROM transactions t
                 JOIN users u ON (t.user_id=u.id)
-                WHERE business_id=%s
+                WHERE t.business_id=%s
             ''', (business_id,))
-            return cur.fetchall()
+            return await cur.fetchall()
